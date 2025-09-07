ac836ed59a5fd6a61207aa161768c68c47a620bd
DataOverlord <53323636+MrDataOverlord@users.noreply.github.com>
2025-09-07 13:40:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ function createDefaultNotificationRules() {
 let transporter = null;
 (async () => {
   if (SMTP_HOST && SMTP_USER && SMTP_PASS) {
-    transporter = nodemailer.createTransporter({
+    transporter = nodemailer.createTransport({
       host: SMTP_HOST,
       port: Number(SMTP_PORT || 465),
       secure: String(SMTP_SECURE || 'true') === 'true',
